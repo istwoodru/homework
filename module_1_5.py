4bf981405a42df6e900159730cb9b9c27022ede3
istwoodru <istwood123@yandex.ru>
2024-09-10 14:32:23 +0700

    Кортеж

---
+++ module_1_5.py
@@ -0,0 +1,5 @@
+immutable_var = (True ,1 ,"namber" ,2.5)
+print("Immutable_var:", immutable_var)
+mutable_list = [2 ,4 ,6, 8]
+mutable_list.append("Modified")
+print( "Mutable_list:" , mutable_list)
