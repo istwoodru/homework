23cd228b45d1cfacc0aee5fdadd44dfdd9eda122
istwoodru <istwood123@yandex.ru>
2024-09-19 00:42:52 +0700

    corrected
    Signed-off-by: istwoodru <istwood123@yandex.ru>

--- module_2_3.py
+++ module_2_3.py
@@ -1,11 +1,14 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7,-6, 5]
-index = 0
-while True:
-    if index < len(my_list) and my_list[index] > 0:
-        print(my_list[index])
-        index += 1
-    elif my_list[index] == 0:
-        index += 1
-    continue
+i = 0
+while i < len(my_list):
+    if my_list[i] > 0:
+        print(my_list[i])
+        i+=1
+        continue
+    elif my_list[i] == 0:
+        i+=1
+    else:
+        break
+
 
 
