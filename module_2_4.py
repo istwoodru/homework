433e97815cd9378593e770898c91cb588ab48200
istwoodru <istwood123@yandex.ru>
2024-10-13 11:15:57 +0700

    easy
    Signed-off-by: istwoodru <istwood123@yandex.ru>

--- module_2_4.py
+++ module_2_4.py
@@ -1,16 +1,23 @@
 numbers  = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
+is_prime = True
 for i in numbers:
-    k = 0
-    for j in range(2, i // 2 + 1):
-       if i % j == 0:
-           k = k + 1
-
-    if k <= 0:
-        print('число простое', i)
+    for k in range(2, len(numbers)):
+        if i % k == 0:
+            is_prime = False
+            break
+    if  is_prime == True and i > 0:
+        primes = primes.append(i)
     else:
-        print('число не простое', i)
+        not_primes = not_primes.append(i)
+print(primes)
+print(not_primes)
+
+
+
+
+
 
 
 
