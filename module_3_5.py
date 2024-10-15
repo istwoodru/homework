fa514e40bf7036a3caac0122f1d0b010b6d3e4f7
istwoodru <istwood123@yandex.ru>
2024-10-16 02:56:41 +0700

    bullshit
    Signed-off-by: istwoodru <istwood123@yandex.ru>

--- module_3_5.py
+++ module_3_5.py
@@ -1,15 +1,15 @@
 def get_multiplied_digits(number):
     str_number = str(number)
     first = int(str_number[0])
-    total = 0
-    if len(str_number) < 1:
+    last = int(str_number[-1])
+    if len(str_number) < 2:
         return first
+    if last == 0:
+        str_number = str_number[:-1]
+        return first * get_multiplied_digits(int(str_number[1:]))
     else:
-        total = first * get_multiplied_digits(int(str_number[1:]))
-        return total
-
-
-result = get_multiplied_digits(40203)
+        return   first * get_multiplied_digits(int(str_number[1:]))
+result = get_multiplied_digits(402030)
 print(result)
 
 
